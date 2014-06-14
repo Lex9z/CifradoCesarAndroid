97ecafcb55144f3129d008e9b60d5893d4e1c4ac
Galileo Teco <genitalico@gmail.com>
2014-06-14 05:12:03 -0500

    MyActivity
    Programada la lógica de la interfaz, EditText, y botón, funcionales

--- app/src/main/java/genitalico/cifradocesar/MyActivity.java
+++ app/src/main/java/genitalico/cifradocesar/MyActivity.java
@@ -4,14 +4,50 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import genitalico.cifradocesar.encrypted.CifradoCesar2;
 
-public class MyActivity extends Activity {
+public class MyActivity extends Activity
+{
+
+    private Button button_accept;
+    private EditText edit_encrypted;
+    private EditText edit_key;
+    private EditText edit_plain_text;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        button_accept = (Button)findViewById(R.id.button_accept);
+        button_accept.setOnClickListener(new android.view.View.OnClickListener()
+        {
+
+            public void onClick(View view)
+            {
+                button_acceptOnClick(view);
+            }
+
+        });
+
+        edit_plain_text = (EditText)findViewById(R.id.edit_plain_text);
+        edit_key = (EditText)findViewById(R.id.edit_key);
+        edit_encrypted = (EditText)findViewById(R.id.edit_encrypted);
+
+        edit_encrypted.setOnFocusChangeListener(new View.OnFocusChangeListener()
+        {
+
+            public void onFocusChange(View view, boolean bol)
+            {
+                button_acceptOnClick(view,bol);
+            }
+
+        });
     }
 
 
@@ -32,5 +68,25 @@ public class MyActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void button_acceptOnClick(View view)
+    {
+        CifradoCesar2 cifradocesar2 = new CifradoCesar2();
+        if (!edit_plain_text.getText().toString().equals("") && !edit_key.getText().toString().equals(""))
+        {
+            cifradocesar2.setCifrar(edit_plain_text.getText().toString(), Integer.parseInt(edit_key.getText().toString()));
+            edit_encrypted.setText(cifradocesar2.getCifrar());
+        }
+    }
+
+    private void button_acceptOnClick(View view,boolean bol)
+    {
+        CifradoCesar2 cifradocesar2 = new CifradoCesar2();
+        if (!edit_plain_text.getText().toString().equals("") && !edit_key.getText().toString().equals(""))
+        {
+            cifradocesar2.setCifrar(edit_plain_text.getText().toString(), Integer.parseInt(edit_key.getText().toString()));
+            edit_encrypted.setText(cifradocesar2.getCifrar());
+        }
     }
 }
